2f2cf6c6590783d5c91301707bd0fb2ab55635ec
Stradivius <nongmanhquynhls@gmail.com>
2023-11-25 19:19:16 +0700

    commit

--- kcbot/examples/drivemode_example.py
+++ kcbot/examples/drivemode_example.py
@@ -8,87 +8,46 @@ from machine import Pin
 buff = 0
 kcbot = KCMORTOR()
 
+# Thay đổi thông số động cơ ở đây, tsdc <= 748
+tsdc = 748
+
+command = {"M": (tsdc,tsdc),
+           "L": (tsdc+25, tsdc-25),
+           "K": (tsdc+50, tsdc-50),
+           "I": (tsdc+75, tsdc-75),
+           "H": (tsdc+100, tsdc-100),
+           "G": (tsdc+125, tsdc-125),
+           "F": (tsdc+150, tsdc-150),
+           "E": (tsdc+175, tsdc-175),
+           "D": (tsdc+200, tsdc-200),
+           "C": (tsdc+225, tsdc-225),
+           "B": (tsdc+250, tsdc-250),
+           "A": (tsdc+275, tsdc-275),
+           "N": (tsdc,tsdc),
+           "O": (tsdc-25, tsdc+25),
+           "P": (tsdc-50, tsdc+50),
+           "Q": (tsdc-75, tsdc+75),
+           "V": (tsdc-100, tsdc+100),
+           "S": (tsdc-125, tsdc+125),
+           "U": (tsdc-150, tsdc+150),
+           "R": (tsdc-175, tsdc+175),
+           "X": (tsdc-200, tsdc+200),
+           "Y": (tsdc-225, tsdc+225),
+           "J": (tsdc-250, tsdc+250),
+           "Z": (tsdc-275, tsdc+275),
+           "1": (0,0),
+           "2": (-tsdc,-tsdc)}
 
 def control():
     global buff
     buff = ble_uart.read().decode().strip()
     if len(buff) > 0:
         buff1 = buff[0]
-        if buff1 == "M":
-            kcbot.motor(1, 800)
-            kcbot.motor(2, 800)
-        if buff1 == "L":
-            kcbot.motor(1, 825)
-            kcbot.motor(2, 775)
-        if buff1 == "K":
-            kcbot.motor(1, 850)
-            kcbot.motor(2, 750)
-        if buff1 == "I":
-            kcbot.motor(1, 875)
-            kcbot.motor(2, 725)
-        if buff1 == "H":
-            kcbot.motor(1, 900)
-            kcbot.motor(2, 700)
-        if buff1 == "G":
-            kcbot.motor(1, 925)
-            kcbot.motor(2, 675)
-        if buff1 == "F":
-            kcbot.motor(1, 950)
-            kcbot.motor(2, 650)
-        if buff1 == "E":
-            kcbot.motor(1, 975)
-            kcbot.motor(2, 625)
-        if buff1 == "D":
-            kcbot.motor(1, 1000)
-            kcbot.motor(2, 600)
-        if buff1 == "C":
-            kcbot.motor(1, 1025)
-            kcbot.motor(2, 575)
-        if buff1 == "B":
-            kcbot.motor(1, 1050)
-            kcbot.motor(2, 550)
-        if buff1 == "A":
-            kcbot.motor(1, 1075)
-            kcbot.motor(2, 525)
-        if buff1 == "N":
-            kcbot.motor(2, 800)
-            kcbot.motor(1, 800)
-        if buff1 == "O":
-            kcbot.motor(2, 825)
-            kcbot.motor(1, 775)
-        if buff1 == "P":
-            kcbot.motor(2, 850)
-            kcbot.motor(1, 750)
-        if buff1 == "Q":
-            kcbot.motor(2, 875)
-            kcbot.motor(1, 725)
-        if buff1 == "V":
-            kcbot.motor(2, 900)
-            kcbot.motor(1, 700)
-        if buff1 == "S":
-            kcbot.motor(2, 925)
-            kcbot.motor(1, 675)
-        if buff1 == "U":
-            kcbot.motor(2, 950)
-            kcbot.motor(1, 650)
-        if buff1 == "R":
-            kcbot.motor(2, 975)
-            kcbot.motor(1, 625)
-        if buff1 == "X":
-            kcbot.motor(2, 1000)
-            kcbot.motor(1, 600)
-        if buff1 == "Y":
-            kcbot.motor(2, 1025)
-            kcbot.motor(1, 575)
-        if buff1 == "J":
-            kcbot.motor(2, 1050)
-            kcbot.motor(1, 550)
-        if buff1 == "Z":
-            kcbot.motor(2, 1075)
-            kcbot.motor(1, 525)
-        if buff1 == "1":
-            kcbot.motorstop();
-        
+        for i in command.keys:
+            if buff1 == i:
+                kcbot.motor(1,command[i][0])
+                kcbot.motor(2,command[i][1]);
+
 
 ble = bluetooth.BLE()
 ble_uart = BLEUART(ble, name="KC_bot_1")
